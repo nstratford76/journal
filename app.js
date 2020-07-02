2bf9f5f98ee0ec38c6ee24afbf969ba50eea2623
nstratford76 <nstratford76@gmail.com>
2020-07-01 23:42:20 -0600

    Work in progress

---
+++ app.js
@@ -0,0 +1,89 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+//const session = require('express-session');
+
+const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MongoDBStore = require('connect-mongodb-session')(session);
+//const csrf = require('csurf');
+//const flash = require('connect-flash');
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://nstratford:oiULZhBv4GScY01R@cluster0-7ss0g.mongodb.net/test";
+const store = new MongoDBStore({
+   uri: MONGODB_URL,
+   collection: 'sessions'
+ });
+
+
+const cors = require('cors'); 
+const User = require('./models/user');
+//const csrfProtection = csrf();
+const corsOptions = {
+  origin: "https://memory-saver.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+
+app.use(
+  session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false,
+    store: store
+   })
+ );
+
+ app.use((req, res, next) => {
+   if (!req.session.user) {
+     return next();
+   }
+  User.findById(req.session.user._id)
+      .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+ });
+//app.use(csrfProtection);
+//app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+   .use(bodyParser({extended: false}))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs');
+  
+   app.use(cors(corsOptions));
+  
+   mongoose
+   .connect(
+     MONGODB_URL, options
+   )
+   .then(result => {
+     app.listen(PORT);
+   })
+   .catch(err => {
+     console.log(err);
+   });
+ 
---
+++ models/entry.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+      type: Date,
+      required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: false
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Entry', entrySchema);
